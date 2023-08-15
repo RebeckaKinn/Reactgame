f207a89478b7547f92b1afbdae83fc4d61b193c2
RebeckaKinn <rebeckaki@getacademy.no>
2023-08-15 15:04:38 +0200

    creating react syntax ins settings

--- Reactgame/src/settings/settings_window.jsx
+++ Reactgame/src/settings/settings_window.jsx
@@ -1,33 +1,44 @@
+import React from 'react';
 
 
+const Settinggs = () => {
 
-const settinggs = () => {
-
-}
-
-const checkIfParent = () => {
-    let parent = false;
-    let number1 = Math.floor(Math.random() * 10);
-    let number2 = Math.floor(Math.random() * 20); 
-    let answer = number1 + number2; 
+    function checkIfParent () {
+        let parent = false;
+        let number1 = Math.floor(Math.random() * 10);
+        let number2 = Math.floor(Math.random() * 20); 
+        let answer = number1 + number2; 
     
-    const firewall = /*HTML*/ `
-    
-        <div>
+        const firewall = (
             <div>
-                <p>${number1}</p>
+              <div>
+                <p>{number1}</p>
                 <p> + </p>
-                <p>${number2}</p>
+                <p>{number2}</p>
+              </div>
+      
+              <input 
+                type="text" 
+                value={userInput}/>
+      
+              <button onClick={() => checkAnswer(userInput, answer)}>aksept</button>
             </div>
-            <button>
-                aksepter
-            </button>
-        </div>
+          );
     
-    `;
-    
-    
-    
+        return firewall;
+    }
+
+    function checkAnswer(userInput, answer) {
+ 
+        if (parseInt(userInput) === answer) {
+
+        } else {
+
+        }
+  }
+
+  return <div>{checkIfParent()}</div>;
+
 }
 
-
+export default Settinggs;
