1bdc842212fef69da40b3844a563fa9f356b7bed
RebeckaKinn <rebeckaki@getacademy.no>
2023-08-15 14:02:26 +0200

    added settings file

---
+++ Reactgame/src/settings/settings_window.jsx
@@ -0,0 +1,33 @@
+
+
+
+const settinggs = () => {
+
+}
+
+const checkIfParent = () => {
+    let parent = false;
+    let number1 = Math.floor(Math.random() * 10);
+    let number2 = Math.floor(Math.random() * 20); 
+    let answer = number1 + number2; 
+    
+    const firewall = /*HTML*/ `
+    
+        <div>
+            <div>
+                <p>${number1}</p>
+                <p> + </p>
+                <p>${number2}</p>
+            </div>
+            <button>
+                aksepter
+            </button>
+        </div>
+    
+    `;
+    
+    
+    
+}
+
+
