88bea526675fbeff1c3c7e560d3cdf2f2d3e53d7
RebeckaKinn <rebeckaki@getacademy.no>
2023-08-24 15:04:19 +0200

    fixed grid placement

--- Reactgame/src/App.jsx
+++ Reactgame/src/App.jsx
@@ -2,21 +2,26 @@ import { useState } from 'react'
 import settings_logo from './assets/settings.png'
 //import background from './assets/moving_cat.gif'
 import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
+    <div className='container'>
       <div>
         <img 
           className='settingsButton'
           src={settings_logo}/>
       </div>
-    <div className='startMenu'>
+      <div 
+        className='startMenu'>
         <h1>velkommen</h1>
-        <div className='startButton'>
-          <button>
-            START
+      </div>
+      <div 
+        className='startButton'>
+        <button>
+          START
         </button>
       </div>
     </div>
