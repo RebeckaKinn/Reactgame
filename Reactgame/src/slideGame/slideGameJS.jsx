2253164c7fdc09c7c4a981c9dc349e98cbbbd86a
RebeckaKinn <rebeckaki@getacademy.no>
2023-09-20 15:17:13 +0200

    comments but works

--- Reactgame/src/slideGame/slideGameJS.jsx
+++ Reactgame/src/slideGame/slideGameJS.jsx
@@ -1,4 +1,5 @@
 //import React from "react"; 
+
 //arrows
 import blueLeft from '../assets/blue_left.png';
 import blueRight from '../assets/blue_right.png';
@@ -11,54 +12,103 @@ import whiteRight from '../assets/white_right.png';
 import image1 from '../assets/tigern.jpg'
 import image2 from '../assets/soppen.jpg'
 
+//TODO: put images in different folders in assets
+//TODO: bildene er ikke centrert
+
 import './slideGame.css';
 
 const cards = [
   { src: image1 },
 
-  { src: image2},
+  { src: image2 },
 
 ];
 
+
+//TODO: Skjekk chatGPT i forhold til useState og update site. Alt fungerer, men ikke oppdatere siden. 
+
+
+let headIndex;
+let bodyIndex;
+let bottomIndex;
+
 function slideGameHandling() {
+
   const randomIndex = () => {
     return Math.floor(Math.random() * cards.length);
   };
 
-  const switchImage = () => {
-
+  const getIndex = () => {
+    headIndex = randomIndex()
+    bodyIndex = randomIndex()
+    bottomIndex = randomIndex()
   }
+  getIndex();
+  
+
+  const switchImage = (direction, index) => {
+    if(direction == 'left'){
+      if(index == 0) index = cards.length - 1;
+      else index--;
+    }
+    else{
+      if(index == cards.length - 1) index = 0;
+      else index++;
+    }
+    console.log(index)
+    return;
+  };
+
+
 
   return (
     <>
     <div className='slideGameBox'>
       <div>
-
-          <img className="arrow" src={blueLeft} alt="blueLeft" />
-
-        <img className="gameImg head" src={cards[randomIndex()].src} alt="head" />
-
-          <img className="arrow" src={blueRight} alt="blueRight" />
-
+        <img 
+            onClick={() => switchImage('left', headIndex)}
+            className="arrow" 
+            src={blueLeft} 
+            alt="blueLeft" />
+          <img 
+              className="gameImg head" 
+              src={cards[headIndex].src} 
+              alt="head" />
+        <img 
+            onClick={() => switchImage('right', headIndex)}
+            className="arrow" 
+            src={blueRight} 
+            alt="blueRight" />
       </div>
 
       <div>
-
-          <img className="arrow" src={yellowLeft} alt="yellowLeft" />
-
-        <img className="gameImg body" src={cards[randomIndex()].src} alt="body" />
-
-          <img className="arrow" src={yellowRight} alt="yellowRight" />
-
+        <img 
+            onClick={() => switchImage('left', bodyIndex)} 
+            className="arrow" src={yellowLeft} 
+            alt="yellowLeft" />
+          <img 
+              className="gameImg body" 
+              src={cards[bodyIndex].src} 
+              alt="body" />
+        <img 
+            onClick={() => switchImage('right', bodyIndex)} 
+            className="arrow" src={yellowRight} 
+            alt="yellowRight" />
       </div>
 
       <div>
-       <img className="arrow" src={whiteLeft} alt="whiteLeft" />
-
-        <img  className="gameImg bottom"src={cards[randomIndex()].src} alt="bottom" />
-
-          <img className="arrow" src={whiteRight} alt="whiteRight" />
-
+       <img 
+          onClick={() => switchImage('left', bottomIndex)} 
+          className="arrow" src={whiteLeft} 
+          alt="whiteLeft" />
+          <img 
+              className="gameImg bottom"
+              src={cards[bottomIndex].src} 
+              alt="bottom" />
+        <img 
+            onClick={() => switchImage('right', bottomIndex)} 
+            className="arrow" src={whiteRight} 
+            alt="whiteRight" />
       </div>
     </div>
     </>
